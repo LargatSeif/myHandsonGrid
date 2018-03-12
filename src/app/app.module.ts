8fd2e81538e901718a5085bc44abf1d7813da02d
abdellaoui_amira <amira.abdellaoui.trabelsi@gmail.com>
2018-03-12 17:59:05 +0200

    Fix style tab cra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,10 @@ import {AccordionModule} from 'primeng/accordion';     //accordion and accordion
 import {MenuItem} from 'primeng/api';                 //api
 */
 import { MomentModule } from 'angular2-moment';
-import { App2Component } from './app2/app2.component';
 
-import { HotTableModule } from '@handsontable/angular';
-import { HotTableRegisterer } from '@handsontable/angular';
+//import { HotTableModule } from '@handsontable/angular';
+
 import { AppComponent } from './app.component';
-import { App3Component } from './app3/app3.component';
 import { NativeComponent } from './native/native.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { MultiselectDropdownModule } from 'angular-2-dropdown-multiselect';
@@ -32,14 +30,11 @@ const routes:Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    App2Component,
-    App3Component,
     NativeComponent
   ],
   imports: [
     BrowserModule,
     DataTableModule,
-    HotTableModule,
     BsDropdownModule.forRoot(),
     MultiselectDropdownModule,
     MomentModule,
